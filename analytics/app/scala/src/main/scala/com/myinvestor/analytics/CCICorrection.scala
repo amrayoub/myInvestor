7da01c5741a77a5675ff6a8a3699ecf4c2d52904
mengwangk <mengwangk@gmail.com>
2016-11-08 23:22:06 +0800

    New files

---
+++ analytics/app/scala/src/main/scala/com/myinvestor/analytics/CCICorrection.scala
@@ -0,0 +1,8 @@
+package com.myinvestor.analytics
+
+/**
+  * Created by MEKOH on 11/8/2016.
+  */
+class CCICorrection {
+
+}
---
+++ analytics/app/scala/src/main/scala/com/myinvestor/analytics/strategy/CCICorrectionStrategy.scala
@@ -0,0 +1,8 @@
+package com.myinvestor.analytics.strategy
+
+/**
+  * Created by MEKOH on 11/8/2016.
+  */
+class CCICorrectionStrategy {
+
+}
