88196433d38799cfe940657d7e73f42b9548e5fe
mengwangk <mengwangk@gmail.com>
2016-11-25 21:20:28 +0800

    updated scripts and files

--- docker/cassandra/control.sh
+++ docker/cassandra/control.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 BUILD_TAG=myinvestor-cassandra
-RUN_NAME=cassandra
+RUN_NAME=myinvestor-db
+DB_DIR=$HOME/$RUN_NAME
+DB_SCRIPT_DIR=$HOME/$RUN_NAME-staging
 
 
 build() {
     echo
-    echo "==== Build ===="
+    echo "==== build ===="
 
     sudo docker build -t $BUILD_TAG .   
 
@@ -18,7 +20,11 @@ start() {
     echo
     echo "==== start ===="
 
-    sudo docker run --name $RUN_NAME -d $BUILD_TAG   
+	# Create folder if not exists
+	[ -d $DB_DIR ] || mkdir -p $DB_DIR
+	[ -d $DB_SCRIPT_DIR ] || mkdir -p $DB_SCRIPT_DIR
+
+    sudo docker run --name $RUN_NAME -v $DB_DIR:/var/lib/cassandra -v $DB_SCRIPT_DIR:/staging -d -p 7199:7199 -p 7000:7000 -p 7001:7001 -p 9160:9160 -p 9042:9042 $BUILD_TAG   
 
     echo    
 }
@@ -29,8 +35,42 @@ stop() {
     echo "==== stop ===="
 
     sudo docker stop $RUN_NAME   
+    sudo docker rm $RUN_NAME   
 
     echo    
+}
+
+status() {
+
+    echo
+    echo "==== status ===="
+
+    sudo docker ps -a | grep $RUN_NAME
+
+    echo    
+}
+
+cqlsh() {
+
+    echo
+    echo "==== cqlsh ===="
+
+	# Create folder if not exists
+	[ -d $DB_SCRIPT_DIR ] || mkdir -p $DB_SCRIPT_DIR
+
+	sudo docker run -it -v $DB_SCRIPT_DIR:/staging --link $RUN_NAME:cassandra --rm cassandra sh -c 'exec cqlsh "$CASSANDRA_PORT_9042_TCP_ADDR"'
+	
+	echo
+}
+
+command() {
+
+    echo
+    echo "==== command ===="
+
+	sudo docker exec -it $RUN_NAME bash
+	
+	echo
 }
 
 
@@ -42,8 +82,7 @@ case "$1" in
             start
             ;;
     'stop')
-            stop ; echo "Sleeping..."; sleep 1 ;
-            start
+            stop 
             ;;
     'status')
             status
@@ -52,9 +91,15 @@ case "$1" in
             stop ; echo "Sleeping..."; sleep 1 ;
             start
             ;;
+    'cqlsh')
+            cqlsh 
+            ;;
+    'command')
+            command
+            ;;
     *)
             echo
-            echo "Usage: $0 { build | start | stop | restart | status }"
+            echo "Usage: $0 { build | start | stop | restart | status | cqlsh | command }"
             echo
             exit 1
             ;;
