2f457801ad4dcbc76791a71dccaa60f1e62b8bbd
mengwangk <mengwangk@gmail.com>
2016-12-08 11:29:15 +0800

    Updated files

---
+++ myInvestor-analytics/myinvestor-core/src/main/scala/com/myinvestor/TradingEvent.scala
@@ -0,0 +1,8 @@
+package com.myinvestor
+
+/**
+  * Created by MEKOH on 12/7/2016.
+  */
+class TradingEvent {
+
+}
---
+++ myInvestor-analytics/myinvestor-core/src/main/scala/com/myinvestor/cluster/ClusterAwareNodeGuardian.scala
@@ -0,0 +1,8 @@
+package com.myinvestor.cluster
+
+/**
+  * Created by MEKOH on 12/7/2016.
+  */
+class ClusterAwareNodeuardian {
+
+}
